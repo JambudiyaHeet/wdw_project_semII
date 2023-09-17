ea8535906729be440c6ab202e0a39f687ee1fa98
Jambudiya Heet <120044678+JambudiyaHeet@users.noreply.github.com>
2023-09-17 14:14:17 +0530

    Updated tik-tak-toe.js
    Added all conditions to check if any of both user has won

--- tik-tak-toe.js
+++ tik-tak-toe.js
@@ -1,4 +1,5 @@
 function make_buttons() {
+    
     for(i=0;i<9;i++){
         var ul = document.getElementById("list");
         var li = document.createElement("button");
@@ -14,27 +15,50 @@ function pageloaded(){
 }
 
 function Mark(id){
-    if( turn%2){
-        turn++ ;
+    if( turn%2 && document.getElementById(id).innerHTML=='_'){
         document.getElementById(id).innerHTML = "X" ;
-        if(check_win("X")){
+        if ( (turn>4) && (check_win("X")) ){
             //Animate a concluding statement about winner 
+            console.log('yup');
         }
+        turn++ ;
+    }
+    else if( turn%2==0 && document.getElementById(id).innerHTML=='_'){
+        document.getElementById(id).innerHTML = "O" ;
+        if( (turn>4) && check_win("O") ){
+            //Animate a concluding statement about winner 
+            console.log('yup');
+        }
+        turn++ ;
     }
     else{
-        turn++ ;
-        document.getElementById(id).innerHTML = "O" ;
-        if(check_win("O")){
-            //Animate a concluding statement about winner 
-        }
+        console.log('invalid choice');
     }
 }
 
 function check_win(sign){
-    var i ;
-    for( i=0;i<3;i++){
-        if(
+    // var content =document.getElementById("0").innerHTML;
+    // console.log(content,sign);
+    // var i ;
+    for( i=0;i<7;i++){
+        if( (document.getElementById(i).innerHTML==sign) && (document.getElementById(i+1).innerHTML==sign)
+             && (document.getElementById(i+2).innerHTML==sign) ){
+                return true;
+        }
     }
+    for(i=0;i<3;i++){
+        if((document.getElementById(i).innerHTML==sign) && (document.getElementById(i+3).innerHTML==sign)
+             && (document.getElementById(i+6).innerHTML==sign)){
+                return true;
+        }
+    }
+    if( (document.getElementById(0).innerHTML==sign) && (document.getElementById(4).innerHTML==sign)
+         && (document.getElementById(8).innerHTML==sign) ){
+            return true;
+    }
+    if( (document.getElementById(2).innerHTML==sign) && (document.getElementById(4).innerHTML==sign)
+         && (document.getElementById(6).innerHTML==sign) ){
+            return true;
+    }
+    return false ;
 }
-
-
